b94f51971a9486685baa776b6fb117aad3971c1d
WilMay <wmayhood@outlook.com>
2024-10-15 20:23:11 -0400

    created an event listener on the search bar form.  The zipcode entered by the user is now sanitized and the proper weather data is displayed

--- src/index.js
+++ src/index.js
@@ -5,5 +5,29 @@ import { updateDOM } from "./updateDOM";
 displayWeather();
 updateDOM('07945');
 
+document.getElementById('searchForm').addEventListener('submit',(event) =>{
+
+    event.preventDefault();
+    const searchedItem = document.getElementById('searchInput').value;
+    
+
+    if(checkCode(searchedItem)){
+        updateDOM(searchedItem.trim());
+
+    }else{
+
+        alert('Please enter a valid 5-digit zip code');
+    }
+
+});
+
+const checkCode = (searchedItem) =>{
+
+    const correctPattern = /^\d{5}$/;
+    return correctPattern.test(searchedItem);
+
+
+}
+
 
 
--- src/updateDOM.js
+++ src/updateDOM.js
@@ -27,21 +27,11 @@ export const updateDOM = async (zipCode) =>{
         humidity.querySelector('.stat').textContent = `${currentWeather.humidity}`;
         
 
+        console.log("dom updated");
+
+
+
         
-
-
-
-        // cloudCover: data.days[0].cloudcover,
-        // feelslike: data.days[0].feelslike,
-        // windSpeed: data.days[0].windspeed,
-        // humidity: data.days[0].humidity,
-        // conditions: data.days[0].conditions,
-        // icon: data.days[0].icon,
-        // temperature: data.days[0].temp,
-        // tempMax: data.days[0].tempmax,
-        // tempMin: data.days[0].tempmin,
-        // sunRise: data.days[0].sunrise,
-        // sunSet: data.days[0].sunset
     }catch(error){
 
 
